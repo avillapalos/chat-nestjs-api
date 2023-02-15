afe78f2072b27decef5bf378466e1e167e0f8441
Alex Villapalos <avillapalos@gmail.com>
2023-02-15 10:33:40 +0100

    [Docker] Dockerfile, docker-compose-local.yml created, and a bash file to interact easily with the containers.

---
+++ local.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+help()
+{
+   echo ""
+   echo "----- CHAT LOCAL SERVER SCRIPTS -----"
+   echo "Usage: $0 <option>"
+   echo "Example: $0 start"
+   echo "Options:"
+   echo -e " - start : Starts the server as daemon"
+   echo -e " - stop : Stops the server"
+   echo -e " - restart : Stops and starts server"
+   echo -e " - test : Runs the tests"
+   echo -e " - coverage : Runs the tests with coverage"
+   echo -e " - shell : Runs an interactive shell on the container"
+   echo "-------------------------------------"
+   echo ""
+   exit 1 # Exit script after printing help
+}
+
+start()
+{
+  docker-compose -f docker-compose-local.yml up dev -d
+}
+
+stop()
+{
+  docker-compose -f docker-compose-local.yml down
+}
+
+restart()
+{
+  stop
+  start
+}
+
+test()
+{
+  start
+  docker exec -it chat_nestjs_api_dev /bin/sh -c "npm run test"
+}
+
+coverage()
+{
+  start
+  docker exec -it chat_nestjs_api_dev /bin/sh -c "npm test -- --coverage"
+}
+
+shell()
+{
+  start
+  docker exec -it chat_nestjs_api_dev /bin/sh
+}
+
+case "$1" in
+  start ) start ;;
+  stop ) stop ;;
+  restart ) restart ;;
+  test ) test ;;
+  coverage ) coverage ;;
+  shell ) shell ;;
+  * ) help ;; # Print helpFunction in case parameter is non-existent
+esac
