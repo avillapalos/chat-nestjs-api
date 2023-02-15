c592984b8df77de17d9e5878401f0c9249967314
Alex Villapalos <avillapalos@gmail.com>
2023-02-15 20:01:33 +0100

    [OpenAPI] Added Swagger built.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Chat API')
+      .setDescription('API for company\'s chat')
+      .setVersion('1.0')
+      .addTag('chat')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
