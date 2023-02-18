fb3364ba8bdb51da391e5883893e53fdf9435a20
Villapalos <avillapalos@gmail.com>
2023-02-18 11:23:40 +0100

    Created DB module and provider

---
+++ src/app/db.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm'
+
+export const databaseProviders = [
+  {
+    provide: 'POSTGRES_DB',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'postgres',
+        port: 5432,
+        username: 'chat',
+        password: 'chat',
+        database: 'chat-db',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      })
+
+      return dataSource.initialize()
+    },
+  },
+]
