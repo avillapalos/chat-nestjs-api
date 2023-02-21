a67568419935857786a44626a226c739e6a14ddf
Villapalos <avillapalos@gmail.com>
2023-02-21 09:54:12 +0100

    Get List Messages of a Room endpoint: full logic and tests

--- src/message/infrastructure/persistence/db.message.repository.ts
+++ src/message/infrastructure/persistence/db.message.repository.ts
@@ -7,4 +7,14 @@ export class DbMessageRepository implements MessageRepository {
   async createMessage(message: Message): Promise<Message> {
     return this.repository.save(message)
   }
+  async getListMessage(roomId: string, limit: number): Promise<Message[]> {
+    return await this.repository
+      .createQueryBuilder('chat-message')
+      .leftJoinAndSelect('chat-message.userId', 'chat-user')
+      .leftJoinAndSelect('chat-message.roomId', 'chat-room')
+      .where('room_id =:roomId', { roomId })
+      .limit(limit)
+      .orderBy('created', 'DESC')
+      .getMany()
+  }
 }
