80b679abae6a86379d1dfb2acd69028a2cf3dbac
Villapalos <avillapalos@gmail.com>
2023-02-21 12:59:48 +0100

    Fix for many to many issue

--- src/room/core/infrastructure/persistence/db.room.repository.ts
+++ src/room/core/infrastructure/persistence/db.room.repository.ts
@@ -3,28 +3,17 @@ import { RoomRepository } from '../../domain/room.repository'
 import { Repository } from 'typeorm'
 import { UserId } from '../../../../user/core/domain/user-id.value-object'
 import { RoomId } from '../../domain/room-id.value-object'
-import { User } from '../../../../user/domain/user.entity'
 
 export class DbRoomRepository implements RoomRepository {
-  constructor(
-    private roomRepository: Repository<Room>,
-    private userRepository: Repository<User>,
-  ) {}
+  constructor(private roomRepository: Repository<Room>) {}
   createRoom(room: Room): Promise<Room> {
     return this.roomRepository.save(room)
   }
   async addUser(roomId: RoomId, userId: UserId): Promise<void> {
-    const room = await this.roomRepository.findOne({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      where: { id: roomId },
-    })
-    const user = await this.userRepository.findOne({
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      where: { id: userId },
-    })
-    room.users = [user]
-    await this.roomRepository.save(room)
+    return await this.roomRepository
+      .createQueryBuilder('rooms-users')
+      .relation(Room, 'users')
+      .of(roomId.value)
+      .add(userId.value)
   }
 }
--- src/room/infrastructure/room.module.ts
+++ src/room/infrastructure/room.module.ts
@@ -9,9 +9,6 @@ import { DatabaseModule } from '../../app/db.module'
 import { DbRoom } from '../core/infrastructure/db.room.entity'
 import { DataSource, Repository } from 'typeorm'
 import { Room } from '../core/domain/room.entity'
-import { USER_TYPEORM_REPOSITORY_TOKEN } from '../../user/infrastructure/user.module'
-import { User } from '../../user/domain/user.entity'
-import { DbUser } from '../../user/infrastructure/db.user.entity'
 
 export const ROOM_REPOSITORY_TOKEN = Symbol('ROOM_REPOSITORY_TOKEN')
 export const ROOM_TYPEORM_REPOSITORY_TOKEN = Symbol(
@@ -24,20 +21,13 @@ export const ROOM_TYPEORM_REPOSITORY_TOKEN = Symbol(
   providers: [
     {
       provide: ROOM_REPOSITORY_TOKEN,
-      useFactory: (
-        roomOrmRepository: Repository<Room>,
-        userOrmRepository: Repository<User>,
-      ) => new DbRoomRepository(roomOrmRepository, userOrmRepository),
-      inject: [ROOM_TYPEORM_REPOSITORY_TOKEN, USER_TYPEORM_REPOSITORY_TOKEN],
+      useFactory: (roomOrmRepository: Repository<Room>) =>
+        new DbRoomRepository(roomOrmRepository),
+      inject: [ROOM_TYPEORM_REPOSITORY_TOKEN],
     },
     {
       provide: ROOM_TYPEORM_REPOSITORY_TOKEN,
       useFactory: (dataSource: DataSource) => dataSource.getRepository(DbRoom),
-      inject: ['POSTGRES_DB'],
-    },
-    {
-      provide: USER_TYPEORM_REPOSITORY_TOKEN,
-      useFactory: (dataSource: DataSource) => dataSource.getRepository(DbUser),
       inject: ['POSTGRES_DB'],
     },
     {
